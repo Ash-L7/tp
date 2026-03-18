cf234b77b39c9a7e7f187f06180ee5c6247ceae3
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-03-19 07:33:36 +0800

    Add support for list tour

---
+++ src/main/java/seedu/address/logic/commands/TourListCommand.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.Model;
+import seedu.address.model.contact.Contact;
+import seedu.address.model.tour.Tour;
+
+/**
+ * Lists all unique tours in the address book to the user.
+ */
+public class TourListCommand extends Command {
+
+    public static final String COMMAND_WORD = "tour_list";
+
+    public static final String MESSAGE_SUCCESS = "Listed all tours";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        Set<Tour> allTours = new HashSet<>();
+        for (Contact contact : model.getAddressBook().getContactList()) {
+            allTours.addAll(contact.getTours());
+        }
+        StringBuilder sb = new StringBuilder(MESSAGE_SUCCESS + ":\n");
+        for (Tour tour : allTours) {
+            sb.append(tour.getTourName()).append("\n");
+        }
+        return new CommandResult(sb.toString());
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.TourAddCommand;
 import seedu.address.logic.commands.TourDeleteCommand;
+import seedu.address.logic.commands.TourListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -81,9 +82,6 @@ public class AddressBookParser {
 
         case TourAddCommand.COMMAND_WORD:
             return new TourAddCommandParser().parse(arguments);
-
-        case TourDeleteCommand.COMMAND_WORD:
-            return new TourDeleteCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
